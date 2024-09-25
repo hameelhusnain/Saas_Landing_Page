75cd76e7f01ad8583e1a57dadda48c8d5b6c0597
hameelhusnain <husnainhameel3@gmail.com>
2024-09-25 13:16:43 +0500

    commits

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,10 +1,9 @@
-import logo from '../../public/images/'
 const Header = () => {
   return (
     <header className="fixed top-0 z-50 w- full py-10">
         <div className="container flex h-14  items-center max-lg:px-5">
             <a className="lg:hidden flex-1 cursor-pointer z-2">
-                <img src="../../public/images/" width={155} height={55} alt="logo" />
+                <img src="./images/svg/xora.svg" width={155} height={55} alt="logo" />
             </a>
             <div className="w-full border-2 border-amber-400">
 
