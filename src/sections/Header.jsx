61494bfb37ca1661997f43b3e670f213cac41cf1
hameelhusnain <husnainhameel3@gmail.com>
2024-09-25 13:45:39 +0500

    Add react scrolll

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,11 +1,24 @@
+import { link as LinkScroll } from 'react-scroll'
+const Navlink = () => (
+  <LinkScroll>
+  
+  </LinkScroll>
+)
 const Header = () => {
   return (
     <header className="fixed top-0 z-50 w- full py-10">
         <div className="container flex h-14  items-center max-lg:px-5">
             <a className="lg:hidden flex-1 cursor-pointer z-2">
-                <img src="./public/images/svg/xora.svg" width={155} height={55} alt="logo" />
+                
             </a>
             <div className="w-full border-2 border-amber-400">
+              <nav>
+                <ul className="flex max-lg:block max-lg:px-12">
+                  <li className="nav-li">
+                    <Navlink/>
+                  </li>
+                </ul>
+              </nav>
 
             </div>
         </div>
